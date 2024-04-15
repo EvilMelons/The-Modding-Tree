2bbc090124022488590b02716fa0745bfd200d9e
EvilMelons <hmpenguin9@gmail.com>
2024-04-14 20:28:30 -0400

    gen

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("Skills", {
     baseResource: "souls", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 0.65, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -29,7 +29,7 @@ addLayer("Skills", {
         11: {
         title: "Speed.",
         description: "Move faster, so that you can kill faster.",
-        cost: new Decimal(10)
+        cost: new Decimal(1)
         },
         12: {
 
